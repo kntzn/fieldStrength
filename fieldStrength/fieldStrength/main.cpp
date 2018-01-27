17f414fa76d63698e6063ff852a834bde3ffd307
kntzn <kunitzyn.art@yandex.ru>
2018-01-27 18:05:37 +0400

    [Imp]: improved visualization

--- fieldStrength/fieldStrength/main.cpp
+++ fieldStrength/fieldStrength/main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <list>
 
-#define K 1000000
+#define K 9000000000 // = [N*m^2/C^2]
 
 float getVecL(sf::Vector2f vec)
 	{
@@ -25,7 +25,7 @@ class Charge
 			sf::CircleShape cs;
 			cs.setPosition(r);
 			cs.setFillColor(sf::Color(255*(charge > 0), 255*(charge < 0), 255*(charge < 0)));
-			cs.setRadius(pow (fabs (charge), 0.33333f));
+			cs.setRadius(pow (fabs (charge)*100, 0.33333f));
 			cs.setOrigin(cs.getRadius() / 2.f, cs.getRadius() / 2.f);
 			window.draw(cs);
 			}
@@ -45,57 +45,61 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(1600, 900), "Electric field strength");
 	
 	std::list <Charge> charges;
-	//charges.push_back (Charge (sf::Vector2f (910, 410),  10));
-	//charges.push_back (Charge (sf::Vector2f (710, 410), -10));
-
+	
 	float newCharge_charge = 0;
+	float maxStrength = 10000000;
 
-	while (window.isOpen())
+	while (window.isOpen ())
 		{
 		sf::Event windowEvent;
 
 		sf::Vector2i MousePos = sf::Mouse::getPosition (window);
 		sf::Vector2f PointerPos = window.mapPixelToCoords (MousePos);
 
+
 		while (window.pollEvent (windowEvent))
 			{
-			if (windowEvent.type == sf::Event::Closed) 
+			if (windowEvent.type == sf::Event::Closed)
 				window.close ();
 			if (windowEvent.type == sf::Event::MouseButtonPressed)
-				charges.push_back (Charge (PointerPos, newCharge_charge));
+				{
+				if (windowEvent.key.code == sf::Mouse::Left)
+					{
+					charges.push_back (Charge (PointerPos, newCharge_charge));
+					}
+				}
 			}
 		// Mouse
 
-		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Equal))
-			newCharge_charge += 1.f;
-		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Dash))
-			newCharge_charge -= 1.f;
+		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Equal) && newCharge_charge <  1)
+			newCharge_charge += 0.01f;
+		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Dash)  && newCharge_charge > -1)
+			newCharge_charge -= 0.01f;
 
 		// Graphics
-		window.clear();
-
-		for (unsigned int x = 0; x < window.getSize().x; x += 20)
-			for (unsigned int y = 0; y < window.getSize().y; y += 20)
+		window.clear ();
+		for (unsigned int x = 0; x < window.getSize ().x; x += 20)
+			for (unsigned int y = 0; y < window.getSize ().y; y += 20)
 				{
 				sf::Vector2f dir;
 				for (auto a : charges)
-					{
-					dir -= K*a.getCharge()/pow (getVecL (sf::Vector2f (x, y) - a.getR()), 3.f) * (a.getR() - sf::Vector2f(x, y));	
-					}
-				dir /= getVecL(dir) / 20;
-
+					dir += K*a.getCharge ()/pow (getVecL (sf::Vector2f (x, y) - a.getR ()), 3.f) * (sf::Vector2f (x, y) - a.getR ());
+					
+				float strength = getVecL (dir);
+				dir /= strength / 20;
 				sf::Vertex line [] =
 					{
-					sf::Vertex(sf::Vector2f(x, y), sf::Color (255, 255, 255, 127)),
-					sf::Vertex(sf::Vector2f(x, y)+dir, sf::Color(255, 255, 255, 127))
+					sf::Vertex (sf::Vector2f (x, y),     sf::Color (255, 127, 127, 255*pow (strength/maxStrength, 0.333f))),
+					sf::Vertex (sf::Vector2f (x, y)+dir, sf::Color (127, 255, 255, 255*pow (strength/maxStrength, 0.333f)))
 					};
-				window.draw(line, 2, sf::PrimitiveType::LineStrip);
+				window.draw (line, 2, sf::PrimitiveType::LineStrip);
 				}
+
 		
 		sf::CircleShape cs;
 		cs.setPosition (PointerPos);
 		cs.setFillColor (sf::Color (0, 255, 0));
-		cs.setRadius (pow (fabs (newCharge_charge), 0.33333f));
+		cs.setRadius (pow (fabs (newCharge_charge)*100, 0.33333f));
 		cs.setOrigin (cs.getRadius () / 2.f, cs.getRadius () / 2.f);
 		window.draw (cs);
 
