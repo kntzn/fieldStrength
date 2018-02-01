74195f27394c290c5746d1411c10f98433f9a968
kntzn <kunitzyn.art@yandex.ru>
2018-02-01 12:34:39 +0300

    [Add]: charge info text [Fix]: overlapping strength vectors [Imp]: field now redraws only if changes occur

--- fieldStrength/fieldStrength/main.cpp
+++ fieldStrength/fieldStrength/main.cpp
@@ -1,7 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <list>
+#include <sstream>
 
 #define K 9000000000 // = [N*m^2/C^2]
+
+#define fieldVisStep 20
 
 float getVecL(sf::Vector2f vec)
 	{
@@ -20,14 +23,23 @@ class Charge
 			charge = CHRG;
 			}
 
-		void draw(sf::RenderWindow &window)
+		void draw(sf::RenderWindow &window, sf::Text &text, bool newCharge = false)
 			{
+			// Charge circle
 			sf::CircleShape cs;
 			cs.setPosition(r);
-			cs.setFillColor(sf::Color(255*(charge > 0), 255*(charge < 0), 255*(charge < 0)));
+			cs.setFillColor(sf::Color(255*(charge > 0 && !newCharge), 255*(charge < 0 || newCharge), 255*(charge < 0 && !newCharge)));
 			cs.setRadius(pow (fabs (charge)*100, 0.33333f));
 			cs.setOrigin(cs.getRadius() / 2.f, cs.getRadius() / 2.f);
 			window.draw(cs);
+
+			// Charge info text
+			std::ostringstream str;
+			str << charge << " C";
+			text.setString(str.str());
+			text.setPosition(r + sf::Vector2f(10, 10));
+			text.setFillColor(cs.getFillColor ());
+			window.draw(text);
 			}
 
 		float getCharge()
@@ -43,11 +55,19 @@ class Charge
 int main()
 	{
 	sf::RenderWindow window(sf::VideoMode(1600, 900), "Electric field strength");
-	
 	std::list <Charge> charges;
+	window.setMouseCursorVisible(false);
 	
 	float newCharge_charge = 0;
-	float maxStrength = 10000000;
+	const float maxStrength = 10000000;
+
+	sf::Font font;
+	font.loadFromFile ("arial.ttf");
+	sf::Text text ("", font, 12);
+
+	sf::Vector2u windowSize = window.getSize ();
+	sf::Texture texture;
+	texture.create (windowSize.x, windowSize.y);
 
 	while (window.isOpen ())
 		{
@@ -56,6 +76,7 @@ int main()
 		sf::Vector2i MousePos = sf::Mouse::getPosition (window);
 		sf::Vector2f PointerPos = window.mapPixelToCoords (MousePos);
 
+		bool updateFlag = false;
 
 		while (window.pollEvent (windowEvent))
 			{
@@ -66,10 +87,10 @@ int main()
 				if (windowEvent.key.code == sf::Mouse::Left)
 					{
 					charges.push_back (Charge (PointerPos, newCharge_charge));
+					updateFlag = true;
 					}
 				}
 			}
-		// Mouse
 
 		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Equal) && newCharge_charge <  1)
 			newCharge_charge += 0.01f;
@@ -78,33 +99,44 @@ int main()
 
 		// Graphics
 		window.clear ();
-		for (unsigned int x = 0; x < window.getSize ().x; x += 20)
-			for (unsigned int y = 0; y < window.getSize ().y; y += 20)
-				{
-				sf::Vector2f dir;
-				for (auto a : charges)
-					dir += K*a.getCharge ()/pow (getVecL (sf::Vector2f (x, y) - a.getR ()), 3.f) * (sf::Vector2f (x, y) - a.getR ());
-					
-				float strength = getVecL (dir);
-				dir /= strength / 20;
-				sf::Vertex line [] =
-					{
-					sf::Vertex (sf::Vector2f (x, y),     sf::Color (255, 127, 127, 255*pow (strength/maxStrength, 0.333f))),
-					sf::Vertex (sf::Vector2f (x, y)+dir, sf::Color (127, 255, 255, 255*pow (strength/maxStrength, 0.333f)))
-					};
-				window.draw (line, 2, sf::PrimitiveType::LineStrip);
-				}
 
-		
-		sf::CircleShape cs;
-		cs.setPosition (PointerPos);
-		cs.setFillColor (sf::Color (0, 255, 0));
-		cs.setRadius (pow (fabs (newCharge_charge)*100, 0.33333f));
-		cs.setOrigin (cs.getRadius () / 2.f, cs.getRadius () / 2.f);
-		window.draw (cs);
+		// Field strenght
+
+		if (updateFlag)
+			{
+			for (unsigned int x = 0; x < window.getSize().x; x += fieldVisStep)
+				for (unsigned int y = 0; y < window.getSize().y; y += fieldVisStep)
+					{
+					sf::Vector2f dir;
+					for (auto a : charges)
+						dir += K*a.getCharge() / pow(getVecL(sf::Vector2f(x, y) - a.getR()), 3.f) * (sf::Vector2f(x, y) - a.getR());
+
+					float strength = getVecL(dir);
+					dir /= strength / fieldVisStep;
+
+					sf::Vertex line[] =
+						{
+						sf::Vertex(sf::Vector2f(x, y) - dir / 2.f, sf::Color(255, 127, 127, 255 * pow(strength / maxStrength, 0.333f))),
+						sf::Vertex(sf::Vector2f(x, y) + dir / 2.f, sf::Color(127, 255, 255, 255 * pow(strength / maxStrength, 0.333f)))
+						};
+					window.draw(line, 2, sf::PrimitiveType::LineStrip);
+					}
+
+			texture.update(window);
+			}
+		else
+			{
+			sf::Sprite spr;
+			spr.setTexture (texture);
+			window.draw(spr);
+			}
+		// New charge point
+		Charge newCharge(PointerPos, newCharge_charge);
+		newCharge.draw(window, text, true);
 
 		for (auto a : charges)
-			a.draw(window);
+			a.draw(window, text);
+			
 
 		window.display();
 		}
