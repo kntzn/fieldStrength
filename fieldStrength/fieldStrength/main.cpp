dba0ce3c357cd918c2cef024e13569e910769012
kntzn <kunitzyn.art@yandex.ru>
2018-02-07 19:39:14 +0300

    [Add]: new vizualization mode

--- fieldStrength/fieldStrength/main.cpp
+++ fieldStrength/fieldStrength/main.cpp
@@ -4,7 +4,7 @@
 
 #define K 9000000000 // = [N*m^2/C^2]
 
-#define fieldVisStep 20
+#define fieldVisStep 10
 
 float getVecL(sf::Vector2f vec)
 	{
@@ -58,8 +58,9 @@ int main()
 	std::list <Charge> charges;
 	window.setMouseCursorVisible(false);
 	
+	int displayMode = 0;
 	float newCharge_charge = 0;
-	const float maxStrength = 10000000;
+	float maxStrength = 100;
 
 	sf::Font font;
 	font.loadFromFile ("arial.ttf");
@@ -89,13 +90,18 @@ int main()
 					charges.push_back (Charge (PointerPos, newCharge_charge));
 					updateFlag = true;
 					}
+				if (windowEvent.key.code == sf::Mouse::Right)
+					{
+					displayMode = (displayMode + 1) % 2;
+					updateFlag = true;
+					}
 				}
 			}
 
-		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Equal) && newCharge_charge <  1)
-			newCharge_charge += 0.01f;
-		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Dash)  && newCharge_charge > -1)
-			newCharge_charge -= 0.01f;
+		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Equal) && newCharge_charge <  10)
+			newCharge_charge += 0.1f;
+		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Dash)  && newCharge_charge > -10)
+			newCharge_charge -= 0.1f;
 
 		// Graphics
 		window.clear ();
@@ -107,20 +113,48 @@ int main()
 			for (unsigned int x = 0; x < window.getSize().x; x += fieldVisStep)
 				for (unsigned int y = 0; y < window.getSize().y; y += fieldVisStep)
 					{
-					sf::Vector2f dir;
+					float strength = 0;
 					for (auto a : charges)
-						dir += K*a.getCharge() / pow(getVecL(sf::Vector2f(x, y) - a.getR()), 3.f) * (sf::Vector2f(x, y) - a.getR());
-
-					float strength = getVecL(dir);
-					dir /= strength / fieldVisStep;
-
-					sf::Vertex line[] =
-						{
-						sf::Vertex(sf::Vector2f(x, y) - dir / 2.f, sf::Color(255, 127, 127, 255 * pow(strength / maxStrength, 0.333f))),
-						sf::Vertex(sf::Vector2f(x, y) + dir / 2.f, sf::Color(127, 255, 255, 255 * pow(strength / maxStrength, 0.333f)))
-						};
-					window.draw(line, 2, sf::PrimitiveType::LineStrip);
+						strength += K*fabs (a.getCharge()) / pow(getVecL(sf::Vector2f(x, y) - a.getR()), 2.f);
+					
+					if (strength > maxStrength)
+						maxStrength = strength;
 					}
+
+			if (displayMode == 0)
+				for (unsigned int x = 0; x < window.getSize().x; x += fieldVisStep)
+					for (unsigned int y = 0; y < window.getSize().y; y += fieldVisStep)
+						{
+						sf::Vector2f dir;
+						for (auto a : charges)
+							dir += K*a.getCharge() / pow(getVecL(sf::Vector2f(x, y) - a.getR()), 3.f) * (sf::Vector2f(x, y) - a.getR());
+
+						float strength = getVecL(dir);
+						dir /= strength / fieldVisStep;
+
+						sf::Vertex line[] =
+							{
+							sf::Vertex(sf::Vector2f(x, y) - dir / 2.f, sf::Color(255, 0, 0, 255 * pow(strength / maxStrength, 0.2f))),
+							sf::Vertex(sf::Vector2f(x, y) + dir / 2.f, sf::Color(0, 255, 255, 255 * pow(strength / maxStrength, 0.2f)))
+							};
+						window.draw(line, 2, sf::PrimitiveType::LineStrip);
+						}
+			else if (displayMode == 1)
+				for (unsigned int x = 0; x < window.getSize().x; x += fieldVisStep)
+					for (unsigned int y = 0; y < window.getSize().y; y += fieldVisStep)
+						{
+						float strength = 0;
+						for (auto a : charges)
+							strength += K*fabs (a.getCharge()) / pow(getVecL(sf::Vector2f(x, y) - a.getR()), 2.f);
+
+
+						sf::RectangleShape rs;
+						rs.setSize (sf::Vector2f(fieldVisStep, fieldVisStep));
+						rs.setFillColor(sf::Color(255, 255, 255, 255 * pow(strength / maxStrength, 0.5f)));
+						rs.setPosition(x, y);
+
+						window.draw(rs);
+						}
 
 			texture.update(window);
 			}
